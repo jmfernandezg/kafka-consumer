75706c90386af644c6789fff357159bc9e344a62
Jose Fernandez <jmfernandezg.awe@gmail.com>
2024-10-25 12:26:45 -0600

    Section 16

--- service/src/main/kotlin/com/jmfg/service/controller/ProductController.kt
+++ service/src/main/kotlin/com/jmfg/service/controller/ProductController.kt
@@ -1,10 +1,13 @@
 package com.jmfg.service.controller
 
 import net.datafaker.Faker
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("/products")
@@ -14,6 +17,9 @@ class ProductController {
 
     @GetMapping("/{id}")
     fun getProduct(@PathVariable id: String): Map<String, String> {
+        if (Random.nextBoolean()) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found")
+        }
         return mapOf(
             "id" to id,
             "name" to faker.commerce().productName(),
